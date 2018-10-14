96f070690db2642b3d67834f4318af6cc4210ca0
Daniel Panero <panero.daniel@gmail.com>
2018-10-14 15:19:07 +0200

    some improvements in readibility and names

--- autoPostaleClass.js
+++ autoPostaleClass.js
@@ -1,85 +1,113 @@
-window.source = ""
+window.source = "";
 
 class AutoPostale {
-  constructor() {
-    this.url = new URL("http://autopostale.teqmonitoring.com/RTPI-AT/rtpi");
-  }
-
-  async listOfBusStops() {
-    let result = await this.customFetch("GETTARGETS", {});
-    window.source = result.source;
-
-    return result.data.map(i => new BusStop(i));
-  }
-
-  stopMonitor() {
-    return this.customFetch("STOPMONITOR", {});
-  }
-
-  async customFetch(type, data) {
-    const getData = () =>
-      fetch(this.buildUrl(type, data), {
-        method: "GET"
-      });
-
-    let result = await getData();
-
-    let _data = await result.json();
-    
-    if (_data.validity === "KO") {
-      result = await getData();
-      _data = await result.json();
+    constructor() {
+        this.url = new URL("http://autopostale.teqmonitoring.com/RTPI-AT/rtpi");
     }
-    return _data;
-  }
 
-  queryParams(type, data) {
-    return {
-      source: window.source,
-      destination: 0,
-      type,
-      data
-    };
-  }
+    async listOfBusStops() {
+        let result = await this.customFetch("GETTARGETS", {});
+        window.source = result.source;
 
-  buildUrl(type, data) {
-    this.url.search = new URLSearchParams({
-      data: JSON.stringify(this.queryParams(type, data))
-    });
-    return this.url;
-  }
+        return result.data.map(i => new BusStop(i));
+    }
+
+    async customFetch(type, data) {
+        const getData = async () =>
+            (await fetch(this.buildUrl(type, data), {
+                method: "GET"
+            })).json();
+
+        let result = await getData();
+
+        if (result.validity === "KO") {
+            result = await getData();
+            if (result.validity === "KO") {
+                result = await getData();
+            }
+        }
+        return result;
+    }
+
+    queryParams(type, data) {
+        return {
+            source: window.source,
+            destination: 0,
+            type,
+            data
+        };
+    }
+
+    buildUrl(type, data) {
+        this.url.search = new URLSearchParams({
+            data: JSON.stringify(this.queryParams(type, data))
+        });
+        return this.url;
+    }
 }
 
 class BusStop extends AutoPostale {
-  constructor({ Name, Label, Identifiers }) {
-    super();
-    this.name = Name;
-    this.id = Identifiers.map(i => parseInt(i));
-    this.label = Label;
-  }
+    constructor({
+        Name,
+        Label,
+        Identifiers
+    }) {
+        super();
+        this.shortName = Name;
+        this.idBusStop = Identifiers.map(i => parseInt(i));
+        this.name = Label;
+    }
 
-  async listOfBusArriving() {
-    let result = await this.customFetch("GETPREDICTIONS", {
-      parameters: this.id
-    });
-
-    return result.data.map(i => new BusItinerary(i));
-  }
+    async listOfBusArriving() {
+        let result = await this.customFetch("GETPREDICTIONS", {
+            parameters: this.idBusStop
+        });
+        return result.data.map(i => new BusArriving(i));
+    }
 }
 
-class BusItinerary extends AutoPostale {
-  constructor({ Target, Route, Dir, Routing }) {
-    super();
-    this.infoBus = {Target, Route, Dir, Routing};
-  }
+class BusArriving extends AutoPostale {
+    constructor({
+        Target,
+        Route,
+        Dir,
+        Routing,
+        FromTarget,
+        ToTarget
+    }) {
+        super();
+        this.infoBus = {
+            idBusStopSelected: parseInt(Target),
+            idItinerary: parseInt(Route),
+            Direction: Dir,
+            Routing
+        };
+        this.infoDestination = {
+            idBusStopFrom: parseInt(FromTarget),
+            idBusStopTo: parseInt(ToTarget)
+        }
+    }
 
-  async itinerary() {
-    return this.customFetch("GETROUTING", {
-      parameters: this.infoBus.keys()
-    });
-  }
+    async itinerary() {
+        let result = await this.customFetch("GETROUTING", {
+            parameters: Object.values(this.infoBus)
+        });
+        return {
+            data: result.data.map(i => new BusStopArriving(i)),
+            delay: parseInt(result.delay),
+            lastConnection: result.lastcom
+        };
+    }
 }
 
-f = new AutoPostale();
-g = await f.listOfBusStops()
-h = await g[555].listOfBusArriving()
+class BusStopArriving {
+    constructor({
+        Status,
+        UID,
+        Time
+    }) {
+        this.status = parseInt(Status);
+        this.idBusStop = parseInt(UID);
+        this.arrivingTime = Time;
+    }
+}
